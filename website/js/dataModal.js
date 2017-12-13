f589197fc25d8e72917c581073ecd8b4c495b836
ZKeiLin <zlin2016@uw.edu>
2017-12-13 01:30:10 -0800

    Delete dataModal.js

--- website/js/dataModal.js
+++
@@ -1,9 +0,0 @@
-$.getJSON("counties.geojson", function (data) {
-    let DATA = data;
-    DATA.features = data.features.filter(function (a) {
-        return a.properties.city === "Seattle";
-    });
-    
-    console.log(DATA);
-    // count(pointsData, data);
-})
